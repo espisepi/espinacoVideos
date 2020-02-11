d3465279fb271bdce8c39d1a4d2f1ed06615be40
jose angel <sepinaco@gmail.com>
2020-02-11 15:59:20 +0100

    photo360Service: cubeParticles into photo360

--- src/app/engine/textures/photo360.service.ts
+++ src/app/engine/textures/photo360.service.ts
@@ -8,13 +8,15 @@ import {
   Scene,
   ArcRotateCamera,
   Vector3,
-  GizmoManager,
+  PhotoDome,
+  StandardMaterial,
+  VideoTexture,
   Mesh,
-  PBRMaterial,
-  CubeTexture,
+  Color4,
+  MeshBuilder,
   Color3,
-  FreeCamera,
-  PhotoDome
+  PointLight,
+  SolidParticleSystem
 } from 'babylonjs';
 
 @Injectable({
@@ -37,7 +39,74 @@ export class Photo360Service {
     this.scene = new Scene(this.engine);
 
     this.createCameraArc();
+    this.createPhotoDome();
+    // this.createVideo();
+    this.createCubeParticles();
 
+  }
+
+  public createCubeParticles() {
+    
+    let pl = new PointLight('p1', new Vector3(0, 0, 0), this.scene);
+    pl.diffuse = new Color3(1, 1, 1);
+    pl.intensity = 1.0;
+
+    let nb = 160000; // nb of triangles
+    let fact = 100; // cube size
+
+    let myPositionFunction = (particle, i, s) => {
+      particle.position.x = (Math.random() - 0.5) * fact;
+      particle.position.y = (Math.random() - 0.5) * fact;
+      particle.position.z = (Math.random() - 0.5) * fact;
+      particle.rotation.x = Math.random() * 3.15;
+      particle.rotation.y = Math.random() * 3.15;
+      particle.rotation.z = Math.random() * 1.5;
+      particle.color = new Color4(
+        particle.position.x / fact + 0.5,
+        particle.position.y / fact + 0.5,
+        particle.position.z / fact + 0.5, 1.0
+      );
+    };
+
+    // model: triangle
+    const triangle = MeshBuilder.CreateDisc('t', {tessellation: 3, sideOrientation: BABYLON.Mesh.DOUBLESIDE}, this.scene);
+
+    // SPS creation: Immutable {updatable: false}
+    const SPS = new SolidParticleSystem('SPS', this.scene, {updatable: false});
+    SPS.addShape(triangle, nb, {positionFunction: myPositionFunction});
+    const shape = SPS.buildMesh();
+
+    // dispose the model
+    triangle.dispose();
+
+    // SPS mesh animation
+    this.scene.registerBeforeRender(() => {
+      pl.position = this.camera.position;
+      //SPS.mesh.rotation.y += 0.01;
+      // SPS.mesh.rotation.z += 0.005;
+    });
+  }
+
+  public createVideo(): void {
+
+    // Create a material from the video
+    const videoTexture = new VideoTexture(
+      'video',
+      ['assets/textures/babylonjs.mp4', 'assets/textures/babylonjs.webm'],
+      this.scene, true, false);
+    videoTexture.video.play();
+
+    const mat = new StandardMaterial('mat', this.scene);
+    mat.diffuseTexture = videoTexture;
+
+    // Attach the video material to the mesh
+    const plane = Mesh.CreatePlane('plane1', 8, this.scene);
+    plane.scaling.x = 1920 / 1080; // set aspect ratio
+    plane.material = mat;
+
+  }
+
+  public createPhotoDome(): void {
     const dome = new PhotoDome(
       'testdome',
       '/assets/textures/360photo.jpg',
@@ -47,7 +116,6 @@ export class Photo360Service {
       },
       this.scene
     );
-
   }
 
   public createCameraArc(): void {
