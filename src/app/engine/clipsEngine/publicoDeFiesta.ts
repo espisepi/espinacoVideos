c67d7d38a54d5c8ee4dc1de6af0afab71241ed72
jose angel <sepinaco@gmail.com>
2020-02-13 23:39:16 +0100

    creada la carpeta publicoFiestero para añadir ahi todos los objetos que puedan aparecer en una escena

--- src/app/engine/clipsEngine/publicoDeFiesta.ts
+++ src/app/engine/clipsEngine/publicoDeFiesta.ts
@@ -1,5 +1,5 @@
 import { Scenario } from './scenario';
-import { Calavera } from '../clips/zikaZaramay/calavera';
+
 
 // lo suyo es que esto sea una interface y no una implementacion
 export class PublicoDeFiesta {
@@ -12,9 +12,17 @@ export class PublicoDeFiesta {
   }
 
   public crearAlPublico(): void {
-    // Aqui es donde instanciaremos todos los objetos
-    // que se utilizaran en nuestra escena
-    // Ejemplo: const calavera = new Calavera(this.scenario.scene, this.scenario.camera);
+    /*
+    Aqui es donde instanciaremos todos los objetos que
+    se utilizaran en nuestra escena, Ejemplo:
+    import { Calavera } from '../publicoFiestero/calavera';
+    const calavera = new Calavera(this.scenario.scene, this.scenario.camera);
+    */
+
+    
+
+
+
   }
 
 }
